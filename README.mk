809ccd4f376d0da7384a07f8e9e40b4d377d5fa5
arsonal <897386402@qq.com>
2020-03-21 15:41:50 +0800

    第一次commit

---
+++ README.mk
@@ -0,0 +1,21 @@
+#自我介绍
+我叫小a，我的爱好和鸡你太美相同：
+*唱
+*跳
+*rap
+*篮球
+
+我有N多前女友：
+1.新垣结衣
+2.石原里美
+3.迪丽热巴
+4.古力娜扎
+
+我学过的编程语言有`java` `scala` `html`.show you my code:
+````
+public class Main {
+	public static void main(Sjtring[] args) {
+		System.out.println("你好，美女！");
+	}
+}
+````
